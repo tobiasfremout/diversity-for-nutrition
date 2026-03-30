9570fb57a9ee1221efdbac8d4bb3964c8ee3a730
tobiasfremout <tobias.fremout@gmail.com>
2026-03-30 17:09:55 -0500

    add minor changes

--- src/data/load_data.R
+++ src/data/load_data.R
@@ -6,16 +6,22 @@
 # load data
 load_data <- function(DATA_FOLDER) {
   
-  cat(paste0("  [load_data] Loading nutrition data...\n"))
+  log_step("n01", "Loading nutrition data...")
   
   # load nutrition data
   nutr_dat <- safe_read_csv(file.path(DATA_FOLDER, "Tables", "species_nutrition_data.csv"))
 
-  # soil extremes data
-  soil_dat <- safe_read_csv(file.path(DATA_FOLDER, "soil_extremes.csv"))
+  # load soil extremes data
+  # soil_dat <- safe_read_csv(file.path(DATA_FOLDER, "soil_extremes.csv"))
   
+  # get the species for which models were made
+  j <- which(nutr_dat$incl_SDM == 1)
+  species_set <- nutr_dat$species[j]
+  
+  # return of the function
   list(
     nutr_dat = nutr_dat,
-    soil_dat = soil_dat
+    # soil_dat = soil_dat,
+    species_set = species_set
   )
 }
