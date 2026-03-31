1a2b1b79f77e22dfa8c9f71fd02bca73a7829d67
tobiasfremout <tobias.fremout@gmail.com>
2026-03-31 11:20:13 -0500

    add test values

--- test_runner.R
+++ test_runner.R
@@ -4,16 +4,18 @@ library(jsonlite)
 # source functions.R
 source("functions.r")
 
-# put parameters in local environment for easier debugging
-lat = lat
-edible_parts_ID = edible_parts_ID
-food_groups_ID = food_groups_ID
-growth_forms_ID = growth_forms_ID
-species_type = species_type
-within_range = within_range
-incl_tentative = incl_tentative
-SSP = SSP
-language_output = language_output
+# test values
+lon = -49.91
+lat = -26.34
+edible_parts_ID = "7,8"
+food_groups_ID = "3,15"
+growth_forms_ID = "1"
+species_type_ID = "1"
+# soil_con_ID = "1"
+within_range = "yes"
+incl_tentative = "yes"
+SSP = "SSP2"
+language_output = "EN"
 
 # call mainNutrition functions
 mainNutrition(
@@ -22,7 +24,8 @@ mainNutrition(
   edible_parts_ID = edible_parts_ID,
   food_groups_ID = food_groups_ID,
   growth_forms_ID = growth_forms_ID,
-  species_type = species_type,
+  species_type_ID = species_type_ID,
+  # soil_con_ID = soil_con_ID,
   within_range = within_range,
   incl_tentative = incl_tentative,
   SSP = SSP,
