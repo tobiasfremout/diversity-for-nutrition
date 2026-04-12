e257103cfdb8b3c80f894b79d3a11e16d970b3ba
tobiasfremout <tobias.fremout@gmail.com>
2026-04-11 19:11:37 -0500

    fix function parameters

--- nutrition.R
+++ nutrition.R
@@ -14,6 +14,7 @@ source(file.path(R_HOME, "src", "libs.r"))
 source(file.path(R_HOME, "src", "io", "helpers.r"))
 source(file.path(R_HOME, "src", "io", "utils.r"))
 source(file.path(R_HOME, "src", "input", "parse_inputs.r"))
+source(file.path(R_HOME, "src", "input", "get_biome.r"))
 source(file.path(R_HOME, "src", "data", "load_data.r"))
 source(file.path(R_HOME, "src", "data", "load_maps.r"))
 source(file.path(R_HOME, "src", "analysis", "species_analysis.r"))
@@ -21,16 +22,17 @@ source(file.path(R_HOME, "src", "analysis", "species_analysis.r"))
 source(file.path(R_HOME, "src", "output", "report_html.r"))
 
 # main function to call the other functions
-process_nutrition <- function(lon = lon,
-                              lat = lat,
-                              edible_parts_ID = edible_parts_ID,
-                              food_groups_ID = food_groups_ID,
-                              growth_forms_ID = growth_forms_ID,
-                              species_type_ID = species_type_ID,
-                              within_range = within_range,
-                              incl_tentative = incl_tentative,
-                              SSP = SSP,
-                              language_output = language_output) {
+process_nutrition <- function(lon = NULL,
+                              lat = NULL,
+                              edible_parts_ID = NULL,
+                              food_groups_ID = NULL,
+                              growth_forms_ID = NULL,
+                              species_type_ID = NULL,
+                              soil_con_ID = NULL,
+                              within_range = NULL,
+                              incl_tentative = NULL,
+                              SSP = NULL,
+                              language_output = NULL) {
     
     ##############################################################################
     # 1. Parse inputs
@@ -42,6 +44,7 @@ process_nutrition <- function(lon = lon,
       food_groups_ID = food_groups_ID,
       growth_forms_ID = growth_forms_ID,
       species_type_ID = species_type_ID,
+      soil_con_ID = soil_con_ID,
       within_range = within_range,
       incl_tentative = incl_tentative,
       SSP = SSP,
@@ -50,21 +53,34 @@ process_nutrition <- function(lon = lon,
     # print(inputs)
     
     ##############################################################################
-    # 2. Load data
+    # 1. Load data
     ##############################################################################
     data <- load_data(
-      DATA_FOLDER = DATA_FOLDER
+      DATA_FOLDER = DATA_FOLDER,
+      language_output = inputs$language_output
     )
     # print(data)
+    
+    ##############################################################################
+    # 2. Get biome
+    ##############################################################################
+    biome <- get_biome(
+      lon = lon,
+      lat = lat,
+      DATA_FOLDER = DATA_FOLDER
+    )
+    # print(biome)
     
     ##############################################################################
     # 3. Load maps
     ##############################################################################
     maps <- load_maps(
-      species_set = species_set,
+      species_set = data$species_set,
       DATA_FOLDER = DATA_FOLDER,
-      within_range = within_range,
-      SSP = SSP
+      BUCKET_NAME = BUCKET_NAME,
+      within_range = inputs$within_range,
+      SSP = inputs$SSP,
+      biome = biome$biome_name
     )
     # print(maps)
     
@@ -83,6 +99,7 @@ process_nutrition <- function(lon = lon,
     ##############################################################################
     species_filt <- filter_species(
       nutr_dat = data$nutr_dat,
+      soil_dat = data$soil_dat,
       edible_parts = data$edible_parts,
       food_groups = data$food_groups,
       growth_forms = data$growth_forms,
@@ -90,11 +107,13 @@ process_nutrition <- function(lon = lon,
       suitable_species = species_extr$suitable_species,
       n_models = species_extr$n_models,
       edible_parts_ID = inputs$edible_parts_ID,
-      food_groups_ID = food_groups_ID,
-      growth_forms_ID = growth_forms_ID,
-      species_type_ID = species_type_ID,
-      incl_tentative = incl_tentative,
-      language_output = language_output
+      food_groups_ID = inputs$food_groups_ID,
+      growth_forms_ID = inputs$growth_forms_ID,
+      species_type_ID = inputs$species_type_ID,
+      soil_con_ID = inputs$soil_con_ID,
+      biome = biome$biome_name,
+      incl_tentative = inputs$incl_tentative,
+      language_output = inputs$language_output
     )
     
     ##############################################################################
@@ -103,12 +122,13 @@ process_nutrition <- function(lon = lon,
     # vis <- visualize(species_res = species_res)
     
     ##############################################################################
-    # 4. Generate HTML report
+    # 7. Generate HTML report
     ##############################################################################
     generate_html_report(
       nutr_table = species_filt$nutr_table,
       output_result = REPORT_FOLDER,
-      language_output = language_output
+      language_output = inputs$language_output,
+      trans_df = data$trans_df
     )
     
 }
