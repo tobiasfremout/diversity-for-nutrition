d7709367f7d47fcc3633907377e373574ef0b359
tobiasfremout <tobias.fremout@gmail.com>
2026-04-13 10:07:00 -0500

    add print statements

--- split rasters in South and Central America.R
+++ split rasters in South and Central America.R
@@ -24,12 +24,12 @@ cat("unmatched CA countries:\n")
 print(ca_countries[!ca_countries %in% unique(world$NAME_0)])
 
 # presence maps masked by hull
+cat("presence maps masked by hull")
 in_dir  <- "C:/Users/tobia/Dropbox/Diversity for Nutrition/diversity-for-nutrition-data/Data/Maps/Presence-absence masked by hull"
 out_sa  <- "C:/Users/tobia/Dropbox/Diversity for Nutrition/diversity-for-nutrition-data/Data/Maps SA/Presence-absence masked by hull"
 out_ca  <- "C:/Users/tobia/Dropbox/Diversity for Nutrition/diversity-for-nutrition-data/Data/Maps CA/Presence-absence masked by hull"
 files <- list.files(in_dir, pattern = "\\.tif$", full.names = TRUE)
 for (f in files) {
-  print(f)
   r <- rast(f)
   fname <- basename(f)
   crop(r, ext(-82, -34, -56, 13)) |> mask(sa_mask) |> writeRaster(file.path(out_sa, fname), overwrite = TRUE)
@@ -37,12 +37,12 @@ for (f in files) {
 }
 
 # presence maps not masked by hull
+cat("presence maps not masked by hull")
 in_dir  <- "C:/Users/tobia/Dropbox/Diversity for Nutrition/diversity-for-nutrition-data/Data/Maps/Presence-absence"
 out_sa  <- "C:/Users/tobia/Dropbox/Diversity for Nutrition/diversity-for-nutrition-data/Data/Maps SA/Presence-absence"
 out_ca  <- "C:/Users/tobia/Dropbox/Diversity for Nutrition/diversity-for-nutrition-data/Data/Maps CA/Presence-absence"
 files <- list.files(in_dir, pattern = "\\.tif$", full.names = TRUE)
 for (f in files) {
-  print(f)
   r <- rast(f)
   fname <- basename(f)
   crop(r, ext(-82, -34, -56, 13)) |> mask(sa_mask) |> writeRaster(file.path(out_sa, fname), overwrite = TRUE)
@@ -50,12 +50,12 @@ for (f in files) {
 }
 
 # future masked by hull - SSP2
+cat("future masked by hull - SSP2")
 in_dir  <- "C:/Users/tobia/Dropbox/Diversity for Nutrition/diversity-for-nutrition-data/Data/Maps/Future masked by hull/SSP2"
 out_sa  <- "C:/Users/tobia/Dropbox/Diversity for Nutrition/diversity-for-nutrition-data/Data/Maps SA/Future masked by hull/SSP2"
 out_ca  <- "C:/Users/tobia/Dropbox/Diversity for Nutrition/diversity-for-nutrition-data/Data/Maps CA/Future masked by hull/SSP2"
 files <- list.files(in_dir, pattern = "\\.tif$", full.names = TRUE)
 for (f in files) {
-  print(f)
   r <- rast(f)
   fname <- basename(f)
   crop(r, ext(-82, -34, -56, 13)) |> mask(sa_mask) |> writeRaster(file.path(out_sa, fname), overwrite = TRUE)
@@ -63,6 +63,7 @@ for (f in files) {
 }
 
 # future masked by hull - SSP3
+cat("future masked by hull - SSP3")
 in_dir  <- "C:/Users/tobia/Dropbox/Diversity for Nutrition/diversity-for-nutrition-data/Data/Maps/Future masked by hull/SSP3"
 out_sa  <- "C:/Users/tobia/Dropbox/Diversity for Nutrition/diversity-for-nutrition-data/Data/Maps SA/Future masked by hull/SSP3"
 out_ca  <- "C:/Users/tobia/Dropbox/Diversity for Nutrition/diversity-for-nutrition-data/Data/Maps CA/Future masked by hull/SSP3"
@@ -76,26 +77,25 @@ for (f in files) {
 }
 
 # future not masked by hull - SSP2
+cat("future not masked by hull - SSP2")
 in_dir  <- "C:/Users/tobia/Dropbox/Diversity for Nutrition/diversity-for-nutrition-data/Data/Maps/Future/SSP2"
 out_sa  <- "C:/Users/tobia/Dropbox/Diversity for Nutrition/diversity-for-nutrition-data/Data/Maps SA/Future/SSP2"
 out_ca  <- "C:/Users/tobia/Dropbox/Diversity for Nutrition/diversity-for-nutrition-data/Data/Maps CA/Future/SSP2"
 files <- list.files(in_dir, pattern = "\\.tif$", full.names = TRUE)
 for (f in files) {
-  print(f)
   r <- rast(f)
   fname <- basename(f)
   crop(r, ext(-82, -34, -56, 13)) |> mask(sa_mask) |> writeRaster(file.path(out_sa, fname), overwrite = TRUE)
   crop(r, ext(-118, -59, 7, 27)) |> mask(ca_mask) |> writeRaster(file.path(out_ca, fname), overwrite = TRUE)
 }
 
-
 # future not masked by hull - SSP3
+cat("future not masked by hull - SSP3")
 in_dir  <- "C:/Users/tobia/Dropbox/Diversity for Nutrition/diversity-for-nutrition-data/Data/Maps/Future/SSP3"
 out_sa  <- "C:/Users/tobia/Dropbox/Diversity for Nutrition/diversity-for-nutrition-data/Data/Maps SA/Future/SSP3"
 out_ca  <- "C:/Users/tobia/Dropbox/Diversity for Nutrition/diversity-for-nutrition-data/Data/Maps CA/Future/SSP3"
 files <- list.files(in_dir, pattern = "\\.tif$", full.names = TRUE)
 for (f in files) {
-  print(f)
   r <- rast(f)
   fname <- basename(f)
   crop(r, ext(-82, -34, -56, 13)) |> mask(sa_mask) |> writeRaster(file.path(out_sa, fname), overwrite = TRUE)
